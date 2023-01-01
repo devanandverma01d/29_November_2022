be8ee93d96e757f06b0cf1d757339da611130b31
Devanand Verma <devanandverma01d@gmail.come>
2023-01-01 23:47:25 +0530

    new

---
+++ js/script.js
@@ -0,0 +1,5 @@
+/*
+This is 3rd Area where we can write JS code i.e. in an External file
+*/
+// 1st way to log/display JS code
+console.log("Hello from Externan file");
---
+++ js/script1.js
@@ -0,0 +1,13 @@
+/*
+This is 3rd area where we can write JS code i.e in an External file
+
+*/
+/*
+This is 2nd Method to log.display  JS code.
+*/
+//window.alert("Hello from External file");
+
+/*
+this is 3rd method to log/display JS code.
+*/
+document.getElementById('demo3').innerHTML = "This is the content area of h3 tag of body from external file"
